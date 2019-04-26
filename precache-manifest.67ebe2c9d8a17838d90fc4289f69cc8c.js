8cdb0b06149b1a97a33ef5c2624d30a73fb479ef
Brandon <baggatbt@gmail.com>
2019-04-26 11:50:05 -0400

    Updates

---
+++ precache-manifest.67ebe2c9d8a17838d90fc4289f69cc8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9b5630dbd91257cb3ac",
+    "url": "/Face-Recognition-App/static/js/runtime~main.df895599.js"
+  },
+  {
+    "revision": "c32eae9448047c49576d",
+    "url": "/Face-Recognition-App/static/js/main.30397c9d.chunk.js"
+  },
+  {
+    "revision": "fe333bf3473122405415",
+    "url": "/Face-Recognition-App/static/js/2.fc8c81b8.chunk.js"
+  },
+  {
+    "revision": "c32eae9448047c49576d",
+    "url": "/Face-Recognition-App/static/css/main.4b37ac9e.chunk.css"
+  },
+  {
+    "revision": "fe333bf3473122405415",
+    "url": "/Face-Recognition-App/static/css/2.d033abbc.chunk.css"
+  },
+  {
+    "revision": "fd4ab7a35cd8a6a5f6e8276136e93bec",
+    "url": "/Face-Recognition-App/index.html"
+  }
+];
